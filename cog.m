4f8ddf617f7f9a24be39af13317daeebed9a7fca
basvanderspek <48215794+basvanderspek@users.noreply.github.com>
2019-03-12 15:18:37 +0100

    Center of Gravity

--- cog.m
+++ cog.m
@@ -1,54 +1,60 @@
-%IMPORT FLIGHT DATA FILE AND FUEL MOMENTS
-%Cit_par_mat;
-%statespace_EOM;
-%FD = open('FLIGHTDATA.m');
+%IMPORT FLIGHT DATA FILE 
+FD = open('FTISxprt-20190308_125059.mat') ;
+FDvariables = fields(FD.flightdata) ; 
+Time = FD.flightdata.time.data ;
+FU = FD.flightdata.lh_engine_FU.data + FD.flightdata.rh_engine_FU.data ;
+TAS = FD.flightdata.Dadc1_tas.data ;   
+
+%IMPORT FUEL DATA AND OTHER MATLAB
+%Cit_par_mat , statespace_EOM;
 FUELfile = csvread('FUELdata.txt') ;
 
-%Static weights in POUNDS
+%START MASS IN POUNDS
 BEM       =  8060 ;    %Provided Basic Empty Mass in POUNDS
-FUEL0     =  730 ;    %Total Fuel on T=0
-PAYLOAD   =  680 ;    %Total payload in POUNDS
+FUEL0     =  3850 ;    %Total Fuel on T=0
+PAYLOAD   =  sum(KG2P(Mass_pax)) ;    %Total payload in POUNDS
 Mass_ramp =  BEM + FUEL0 + PAYLOAD;
 ZFM       =  BEM + PAYLOAD;
 MAC       =  89.89 ;           %inches
+Weight_nose = 3000;
+Weight_main = 8000;
 
 %START CENTER OF GRAVITY
+D_bem = 300.21 - (218.2*Weight_nose / (Weight_nose+Weight_main));
 D_pax = [131,131,214,214,251,251,288,288,170];   %inch
 D_bag = [74,321,338];                            %inch
-Mass_pax = [0,0,0,0,0,0,0,0,0];               %pounds
-Mass_bag = [0,0,0];                            %pounds
-Moment_pax = D_pax.*Mass_pax;
-Moment_bag = D_bag.*Mass_bag;
-Moment_pay  = sum(Moment_pax) + sum(Moment_bag);
+Mass_pax = [82,92,75,56,63,75,75,76,77];  %KG!!!
+Mass_bag = [0,0,0];                       %KG!!!
 
-Weight_nose = 0;
-Weight_main = 0;
-D_bem = 300.21 - (218.2*Weight_nose / (Weight_nose+Weight_main));
 Moment_BEM  = BEM * D_bem;
-
+Moment_pax = D_pax.*KG2P(Mass_pax);
+Moment_bag = D_bag.*KG2P(Mass_bag);
+Moment_PAY  = sum(Moment_pax) + sum(Moment_bag);
 Moment_FUEL0 = FUEL0 *interp1(FUELfile(:,1),FUELfile(:,2),FUEL0);
 
-Moment_ZFM  = Moment_BEM + Moment_pay;
+Moment_ZFM  = Moment_BEM + Moment_PAY;
 Moment_RAMP = Moment_ZFM + Moment_FUEL0;
 CG_start = Moment_RAMP / Mass_ramp     ;     %defined from nose
 
-%CONTINUOUS MASS AND CG THROUGOUT FLIGHT DATA
-%for i=1:length(FD)
-%    Mass_t(i)= Mass_ramp - integral(flow,0,t);
-%end
+
+%CONTINUOUS MASS AND
+%CONTINUOUS CENTER OF GRAVITY
+for i=1:length(FU)
+    Mass_t(i)= Mass_ramp - FU(i);
+end
 
 
-%CONVERSION FUNCTIONS
-function pounds = kg2p(kg)
+%CONVERSON FUNCTIONS
+function pounds = KG2P(kg)
     pounds = kg / 0.453592;
 end
-function kg = p2kg(pounds)
+function kg = P2KG(pounds)
     kg = pounds * 0.453592;
 end
-function inch = cm2inch(cm)
+function inch = CM2INCH(cm)
     inch = cm/2.54;
 end
-function cm = inch2cm(inch)
+function cm = INCH2CM(inch)
     cm = inch*2.54;
 end
 
