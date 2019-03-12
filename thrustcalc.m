4c5b5b60943ced401db1c0414931a4280357fad6
Bloempje <47719562+Bloempje@users.noreply.github.com>
2019-03-12 16:21:18 +0100

    Add files via upload

--- thrustcalc.m
+++ thrustcalc.m
@@ -1,17 +1,38 @@
 gamma = 1.4 ;   % spec. heat ratio
 R_s = 287.058 ; % [J/kg*K] - spec. gas constant
-T_0 = 200 ;     % [K] - undisturbed outside temp.
-h_p = 10000 ;   % [m] - ISA pressure altitude
 bpr = 2.6 ;     % by-pass ratio
-M_0 = V_0*sqrt(gamma*R_s*T_0) ; % undisturbed Mach number
-m_f1 = 0.177 ;  % [kg/s] - fuel flow
-m_f2 = 0.177 ;  % [kg/s] - fuel flow
-m = 35.245 ;    % [kg/s] - total mass flow
-T_ISA = T_0 + (bpr*h_p) ; % [K] - ISA Temperature
+mf = 35.245 ;   % [kg/s] - total mass flow
 
 % ---------------------------------------------------------------------
 
-data = rand(10,5) ;     % some random data
-data = round(data*1000)/1000 ;
+% measured total temp.
+Tm_t = flightdata.Dadc1_tat.data ; % [C]
+Tm_t = Tm_t + 273.15 ; % [K]
+
+% pressure altitude
+h_p = flightdata.Dadc1_alt.data ; % [ft]
+h_p = h_p./3.2808399 ; % [m]
+
+% fuel flow left engine
+mfl = flightdata.lh_engine_FMF.data ;  % [lbs/h]
+mfl = mfl*(0.45359237/3600) ; % [kg/s]
+
+% fuel flow right engine
+mfr = flightdata.rh_engine_FMF.data ;  % [lbs/h]
+mfr = mfr*(0.45359237/3600) ; % [kg/s]
+
+% Mach number
+M = flightdata.Dadc1_mach.data ; % [M]
+
+% ISA Temperature
+T_ISA = 288.15 + ((-0.0065)*h_p) ; % [K]
+
+% Temp. difference
+dT = T_ISA - Tm_t ; % [K]
+
+% ---------------------------------------------------------------------
+
+% Merging data set - correct format
+data = [h_p M dT mfl mfr] ;
 dlmwrite('matlab.dat',data,'delimiter',' ') 
 load matlab.dat   % load the file
