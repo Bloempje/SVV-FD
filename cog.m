e7bcc23a8f9a1123fefe12afaf9fcb16e105d8d1
basvanderspek <48215794+basvanderspek@users.noreply.github.com>
2019-03-12 17:57:28 +0100

    Update cog.m

--- cog.m
+++ cog.m
@@ -47,6 +47,7 @@ plot(Time,Mass_t,Time,FU);
 FDvariables = (fields(FD.flightdata));    
 FU_total = P2KG(FU(length(FU)));
 
+
 %CONVERSON FUNCTIONS
 function pounds = KG2P(kg)
     pounds = kg / 0.453592;
