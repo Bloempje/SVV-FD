b634fd2d033aee6c69a2199dd737fab0ea78a19b
Bloempje <47719562+Bloempje@users.noreply.github.com>
2019-03-12 17:56:06 +0100

    Update thrustcalcII.m

--- thrustcalcII.m
+++ thrustcalcII.m
@@ -1,57 +1,67 @@
-load flightdata.mat
+g = 9.81 ; % gravitational constant
+gamma = 1.4 ;   % spec. heat ratio
+R = 287.05 ; % [J/kg*K] - spec. gas constant
+bpr = 2.6 ;     % by-pass ratio
+mf = 35.245 ;    % [kg/s] - total mass flow
+
+% -------------------------------------------------------------------
+
+% datasheet
+hp0 = transpose([6990, 7000, 8010, 8000, 8010, 8980]) ;   % [ft]
+Vc = transpose([241, 219, 192, 161, 137, 119]) ;         % [kts]
+TAT = transpose([1.2, -1.2, -2.2, -6.5, -6.2, -7.0]) ;  % [C]
+mfl = transpose([720, 603, 492, 464, 387, 373]) ;       % [lbs/hr]
+mfr = transpose([756, 643, 533, 509, 426, 409]) ;       % [lbs/hr]
 
 % ---------------------------------------------------------------------
 
 % measured total temp.
-Tm_t = TAT ; % [C]
-Tm_t = Tm_t + 273.15 ; % [K]
+TAT = TAT + 273.15 ;    % [K]
 
 % pressure altitude
-h_p = h; % [ft]
-h_p = h_p./3.2808399 ; % [m]
+hp0 = hp0./3.2808399 ;      % [m]
 
 % fuel flow left engine
-mfl = FFl ;  % [lbs/h]
-mfl = mfl*(0.45359237/3600) ; % [kg/s]
+mfl = mfl*(0.45359237/3600) ;   % [kg/s]
 
 % fuel flow right engine
-mfr = FFr ;  % [lbs/h]
-mfr = mfr*(0.45359237/3600) ; % [kg/s]
+mfr = mfr*(0.45359237/3600) ;   % [kg/s]
 
 % Mach number
-M = flightdata.Dadc1_mach.data ; % [M]
 for i=1:6
-g_0 = g ; % gravitational constant
-gamma = 1.4 ;   % spec. heat ratio
-R_s = 287.058 ; % [J/kg*K] - spec. gas constant
-bpr = 2.6 ;     % by-pass ratio
-m_f1 = FFl(i) ;  % [kg/s] - fuel flow left engine
-m_f2 = FFr(i) ;  % [kg/s] - fuel flow right engine
-V_c = V(i) ; % [m/s] - calibrated airspeed
-T_m = TAT(i) ;     % [K] - measured air temp.
+    
+    m_f1 = mfl(i) ;         % [kg/s] - fuel flow left engine
+    m_f2 = mfr(i) ;         % [kg/s] - fuel flow right engine
+    V_c = Vc(i)*0.51444 ;    % [m/s] - calibrated airspeed
+    T_m = TAT(i) ;          % [K] - measured air temp.
 
-h_p = h(i);   % [m] - ISA pressure altitude
-[T_0, a_0, P_0, rho_0] = atmoscoesa(h_p) ; % ISA parameters
+    h_p = hp0(i);             % [m] - ISA pressure altitude
+    [T_isa, a_isa, P_isa, rho_isa] = atmoscoesa(h_p) ;  % ISA parameters
 
-% ---------- Reduction of the airspeed -------------
+    % ---------- Reduction of the airspeed -------------
 
-% static pressure
-P = P_0*(1 + ((bpr*h_p)/T_0))^(g_0/(bpr*R_s)) ;
+    % static pressure
+    P = P_isa*(1 + ((bpr*h_p)/T_isa))^(g/(bpr*R)) ;
 
-% Mach number
-M = sqrt((2/(gamma - 1))*((1 + (P_0/P)*((1 + ((gamma - 1)*rho_0*(V_c)^2)/(2*gamma*P_0))^(gamma/(gamma - 1)) - 1))^((gamma - 1)/gamma) - 1)) ;
+    % Mach number
+    Mch = sqrt((2/(gamma - 1))*((1 + (P_isa/P)*((1 + ((gamma - 1)*rho_isa*(V_c)^2)/(2*gamma*P_isa))^(gamma/(gamma - 1)) - 1))^((gamma - 1)/gamma) - 1)) ;
 
-M(i) = M;
+    M(i) = Mch;
+
 end
+
+Mach = transpose(M) ;
+
 % ISA Temperature
-T_ISA = 288.15 + ((-0.0065)*h_p) ; % [K]
+T_ISA = 288.15 + ((-0.0065)*hp0) ;      % [K]
 
 % Temp. difference
-dT = Tm_t - T_ISA ; % [K]
+dT = TAT - T_ISA ;                      % [K]
 
 % ---------------------------------------------------------------------
 
 % Merging data set - correct format
-data = [h_p M dT mfl mfr] ;
+data = [hp0 Mach dT mfl mfr] ;
+data = round(data*1000)/1000 ;
 dlmwrite('matlab.dat',data,'delimiter',' ') 
 load matlab.dat   % load the file
