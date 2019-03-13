9fe149c827974782f7a2e136d8f050b743ea42c3
basvanderspek <48215794+basvanderspek@users.noreply.github.com>
2019-03-13 11:21:01 +0100

    Continuous COG

--- cog.m
+++ cog.m
@@ -1,7 +1,7 @@
 %% MASS & Center of Gravity Balance MATLAB FILE B15
 %% IMPORTS: MATLAB FILES // FLIGHT DATA FILE // FUEL DATA
-Cit_par_mat , statespace_EOM;                                               %open matlab group file
-FUELfile = csvread('FUELdata.txt') ;                                        %open fuel moment file
+tic , %Cit_par_mat , statespace_EOM;                                        %open matlab group file                                                                                    
+FF = csvread('FUELdata.txt') ;                                              %open fuel moment file
 FD = open('FTISxprt-20190308_125059.mat') ;                                 %open flightdata file
 Time = FD.flightdata.time.data ;                                            %Time in (deci)seconds
 FU = FD.flightdata.lh_engine_FU.data + FD.flightdata.rh_engine_FU.data ;    %FUEL USED
@@ -9,7 +9,7 @@ FU = FD.flightdata.lh_engine_FU.data + FD.flightdata.rh_engine_FU.data ;    %FUE
 TAS = FD.flightdata.Dadc1_tas.data ;                                        %True Airspeed
 stickforce = FD.flightdata.column_fe.data ;                                 %Stickforce
 AOA = FD.flightdata.vane_AOA.data ;                                         %Angle of Attack
-BCalt = FD.flightdata.Dadc1_bcAlt.data ;                                    %Baro corr. alt
+H_BC = FD.flightdata.Dadc1_bcAlt.data ;                                     %Baro corr. alt
 %% START MASS & Xcg 
 BEM  =  9165 ;                                                              %Provided Basic Empty Mass in (POUNDS)
 Mass_fuel0=  3850 ;                                                         %Total Fuel on T=0 (POUNDS)   
@@ -17,11 +17,11 @@ Mass_pax  = KG2P([82,92,56,63,75,75,76,77,75]);                             %Pas
 Mass_bag  = KG2P([0,0,0]);                                                  %Bags Mass (INPUT = KG)
 Mass_pay  = sum(Mass_pax) + sum(Mass_bag) ;                                 %Total payload in POUNDS
 Mass_ZF   =  BEM + Mass_pay;                                                %Zero fuel mass (POUNDS)
-Mass_ramp =  BEM + Mass_pay + Mass_fuel0 ;                                  %Ramp mass T=0 (POUNDS)
+Mass_0 =  BEM + Mass_pay + Mass_fuel0 ;                                     %Ramp mass T=0 (POUNDS)
 
 %START CENTER OF GRAVITY
-W_nose_jack = 1220*4,44822 ;                                                %Weight on nose jack (N)
-W_main_jack = 9945*4,44822 ;                                                %Weight on main jack (N)
+W_nose_jack = 1220*4.44822 ;                                                %Weight on nose jack (N)
+W_main_jack = 9945*4.44822 ;                                                %Weight on main jack (N)
 D_bem_jack = 315.5 - (221.8*W_nose_jack / (W_nose_jack+W_main_jack));       %ARM BEM (INCH)
 D_BEM = 292.18 ;                                                            %ARM BEM provided (INCH)
 Moment_BEM  = BEM * D_BEM;                                                  %Moment BEM (INCH-POUNDS)
@@ -32,57 +32,63 @@ Moment_pax = D_pax.*(Mass_pax);                                             %Mom
 Moment_bag = D_bag.*(Mass_bag);                                             %Moment Bags (INCH-POUNDS)
 Moment_pay  = sum(Moment_pax) + sum(Moment_bag);                            %Moment Payload (INCH-POUNDS)
 
-Moment_fuel0 = 100*interp1(FUELfile(:,1),FUELfile(:,2),Mass_fuel0);         %Moment Fuel (INCH-POUNDS)
+Moment_fuel0 = 100*interp1(FF(:,1),FF(:,2),Mass_fuel0);         %Moment Fuel (INCH-POUNDS)
 
-Moment_RAMP = Moment_BEM + Moment_pay + Moment_fuel0;                       %Moment Ramp T=0 (INCH-POUNDS)
-CG_start = Moment_RAMP/Mass_ramp ;                                          %Xcog (INCH)
+Moment_0 = Moment_BEM + Moment_pay + Moment_fuel0;                          %Ramp T=0 Moment (INCH-POUNDS)
+CG_0 = Moment_0/Mass_0 ;                                                    %Ramp T=0 Xcog (INCH)
 
 %% CONTINUOUS MASS AND  CENTER OF GRAVITY
 for i=1:length(Time)
-    Mass_t(i)= Mass_ramp - FU(i) ;                                          %
-    Mass_fuel_t(i) = Mass_fuel0 - FU(i) ;                                   %
-    Moment_fuel_t(i)= 100*interp1(FUELfile(:,1),FUELfile(:,2),Mass_fuel_t(i))  ; %
-    if (i > ((52.5*60*10)-89)) && (i < ((54.5*60*10)-89))                   %
-        Moment_paxshift(i) = Mass_pax(8)*D_pax(1) - Mass_pax(8)*D_pax(8) ;  %
+    Mass_t(i)= Mass_0 - FU(i) ;                                             %Continuous Mass
+    Mass_fuel_t(i) = Mass_fuel0 - FU(i) ;                                   %Continuous Fuel Mass
+    Moment_fuel_t(i)= 100*interp1(FF(:,1),FF(:,2),Mass_fuel_t(i));          %Continuous Fuel Moment
+    if (i > ((52.5*60*10)-89)) && (i < ((54.5*60*10)-89))                   %IF time in range Pax shift
+        Moment_S(i) = Mass_pax(8)*D_pax(1) - Mass_pax(8)*D_pax(8) ;         %Passenger Shift
     else    
-        Moment_paxshift(i) = 0 ;
+        Moment_S(i) = 0 ;
     end
-    Moment_t(i) = Moment_BEM + Moment_fuel_t(i) + Moment_pay + Moment_paxshift(i) ;
-    CG_t(i) = Moment_t(i)/Mass_t(i) ;
+    Moment_t(i) = Moment_BEM + Moment_fuel_t(i) + Moment_pay + Moment_S(i); %Continuous Moment
+    CG_t(i) = Moment_t(i)/Mass_t(i) ;                                       %Continuous Xcog 
 end   
 
 %% PLOTS
 for i=1:length(Time)
-    BEMline(i) = BEM ;
-    BEMpayline(i) = BEM + Mass_pay;
-    BEMpayFUELline(i) = BEM + Mass_pay + Mass_fuel0;
+    Line_BEM(i) = BEM ;
+    Line_PAY(i) = BEM + Mass_pay;
+    Line_0(i) = BEM + Mass_pay + Mass_fuel0;
 end
-%plot(Time,Mass_t,Time,FU,Time,BEMline,Time,BEMpayline,Time,BEMpayFUELline);
+%plot(Time,Mass_t,Time,FU,Time,Line_BEM,Time,Line_PAY,Time,Line_0);
 %title('Total weight & Fuel consumed - plot')
 
-plot(Time,stickforce,Time,AOA,Time,BCalt/1000) ;
+plot(Time,stickforce,Time,AOA,Time,H_BC/100) ;
 title('Stickforce & AOA & ALT - plot') ;
 
 %plot(Time,CG_t) ;
 title('Xcg plot') ;
-%% SI UNITS EXPORTS
-Mass_kg = P2KG(Mass_t) ; 
-XCG_cm = INCH2CM(CG_t) ; 
 
+%% SI UNITS EXPORTS
+SI_Mass_t = P2KG(Mass_t) ; 
+SI_XCG_t = INCH2CM(CG_t)/100 ; 
+SI_Moment_t = IP2NM(Moment_t) ;
+
+toc                                                                         %Stop Timer
 
 %% CONVERSON FUNCTIONS
-function pounds = KG2P(kg)
-    pounds = kg / 0.453592;
+function pounds = KG2P(kg)                                                  %Kg    -> Lbs      
+    pounds = kg / 0.453592;     
 end
 function kg = P2KG(pounds)
     kg = pounds * 0.453592;
-end
+end                                               %Lbs   -> Kg
 function inch = CM2INCH(cm)
     inch = cm/2.54;
-end
+end                                              %Cm    -> Inch
 function cm = INCH2CM(inch)
     cm = inch*2.54;
-end
+end                                              %Inch  -> Cm
+function NewtonMeter = IP2NM(InchPound)
+     NewtonMeter = InchPound*0.112984829 ;
+end                                  %In-lb -> Nm
 
 
     
